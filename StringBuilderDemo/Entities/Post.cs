d5fade4864e0a226a4189e478b0cc94fd453f5ba
João Pedro <medeirosjpro@hotmail.com>
2022-09-26 10:50:20 -0300

    StringBuilder Demo

---
+++ StringBuilderDemo/Entities/Post.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringBuilderDemo.Entities
+{
+    class Post
+    {   
+        public string  Title { get; set; }
+        public int Likes { get; set; }
+        public DateTime Moment { get; set; }
+        public string Content { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+        public Post()
+        {
+        }
+
+        public Post(string title, int likes, DateTime moment, string content)
+        {
+            Title = title;
+            Likes = likes;
+            Moment = moment;
+            Content = content;
+        }
+
+        public void AddComment(Comment comments)
+        {
+            Comments.Add(comments);
+        }
+        public void RemoveComment(Comment comments)
+        {
+            Comments.Remove(comments);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(Title);
+            sb.Append(Likes);
+            sb.Append(" Likes - ");
+            sb.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine(Content);
+            sb.AppendLine("Comments: ");
+            foreach(Comment c in Comments)
+            {
+                sb.AppendLine(c.Text);
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ StringBuilderDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using StringBuilderDemo.Entities;
+
+namespace StringBuilderDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Comment c1 = new Comment("Have a nice trip");
+            Comment c2 = new Comment("Wow tha's awesome!");
+            Post p1 = new Post(
+
+                "Traveling to New Zealand",
+                 12,
+                DateTime.Parse("21/06/2018 13:05:44"),
+                "I'm going to visit this wonderful country!");
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+
+
+            Comment c3 = new Comment("Good Night");
+            Comment c4 = new Comment("May the Force be with you");
+            Post p2 = new Post(
+
+                "Good night guys",
+                 5,
+                DateTime.Parse("28/07/2018 23:14:19"),
+                "See you tomorrow");
+            p2.AddComment(c3);
+            p2.AddComment(c4);
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+        }
+    }
+}
